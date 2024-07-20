3ef0c96acf50912310b7abbc39d5abd83e6e58be
wabisque <welseywebb35@gmail.com>
2024-07-20 15:42:42 +0000

    restored app.vue

---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+</template>
+
+<style>
+.app {
+  min-block-size: 100dvh;
+  inline-size: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
+import App from './app.vue';
 import router from './router/index.js';
 
 const app = createApp(App);
